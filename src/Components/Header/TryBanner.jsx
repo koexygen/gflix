051c8126c78ecc6384cc3defd76f1b90e980a230
koexygen <rexarkoexygen@gmail.com>
2021-03-26 11:05:38 +0400

    transfer

--- src/Components/Header/TryBanner.jsx
+++ src/Components/Header/TryBanner.jsx
@@ -1,7 +1,14 @@
 import "./TryBanner.scss";
 
 function TryBanner() {
-  return <div>hi there trybanner</div>;
+  return (
+    <div className="try-banner">
+      <h1 className="try-banner-title">
+        Unlimited films, TV programmes and more.
+      </h1>
+      <h2 className="try-banner-subtitle">Watch anywhere. Cancel anytime.</h2>
+    </div>
+  );
 }
 
 export default TryBanner;
