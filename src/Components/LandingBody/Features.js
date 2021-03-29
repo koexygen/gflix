c311d8faee50f17c4a7be2aaf5e3a6fb82ffc3e6
koexygen <rexarkoexygen@gmail.com>
2021-03-29 19:01:12 +0400

    features second part update

--- src/Components/LandingBody/Features.js
+++ src/Components/LandingBody/Features.js
@@ -1,10 +1,27 @@
 import "./Features.scss";
+import TV from "../../Assets/images/tv.png";
+import mobileImg from "../../Assets/images/mobile-0819.jpg";
 import FeaturesItem from "./FeaturesItem";
 
 function Features() {
   return (
     <div className="features">
-      <FeaturesItem />
+      <FeaturesItem
+        title="Enjoy on your TV."
+        description="Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray
+            players, and more."
+        overlayImgSrc={TV}
+        video
+      />
+      <FeaturesItem
+        title="Download your shows to watch offline."
+        description="Save your favorites easily and always have something to watch."
+        direction="row-reverse"
+        overlayImgSrc={mobileImg}
+        customClass="mobile-part"
+        downloadLoader
+        reverseIt
+      />
     </div>
   );
 }
--- src/Components/LandingBody/FeaturesItem.js
+++ src/Components/LandingBody/FeaturesItem.js
@@ -1,34 +1,73 @@
 import "./FeaturesItem.scss";
-import TV from "../../Assets/images/tv.png";
+import videoForTV from "../../Assets/videos/video-tv-0819.m4v";
 
-function FeaturesItem() {
+function FeaturesItem(props) {
   return (
     <div className="features-item">
-      <div className="features-item--container-inside">
+      <div
+        className={`features-item--container-inside ${
+          props.reverseIt ? "custom-reverse-direction" : null
+        }`}
+      >
         <div className="features-item--panel">
-          <h1 className="features-item--panel-title">Enjoy on your TV.</h1>
-          <h2 className="features-item--panel-subtitle">
-            Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray
-            players, and more.
-          </h2>
+          <h1 className="features-item--panel-title">{props.title}</h1>
+          <h2 className="features-item--panel-subtitle">{props.description}</h2>
         </div>
 
         <div className="features-item--panel">
-          <div className="features-item--panel-img-video">
-            <img src={TV} alt="" className="features-item--panel-img" />
-            <video
-              className="our-story-card-video"
-              autoPlay
-              playsInline
-              muted
-              loop
+          {props.video ? (
+            <div
+              className={
+                props.customClass
+                  ? "features-item--panel-img-video " + props.customClass
+                  : "features-item--panel-img-video"
+              }
             >
-              <source
-                src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v"
-                type="video/mp4"
+              <img
+                src={props.overlayImgSrc}
+                alt=""
+                className="features-item--panel-img"
               />
-            </video>
-          </div>
+              <video
+                className="our-story-card-video"
+                autoPlay
+                playsInline
+                muted
+                loop
+              >
+                <source src={videoForTV} type="video/mp4" />
+              </video>
+            </div>
+          ) : (
+            <div
+              className={
+                props.customClass
+                  ? "features-item--panel-img-video " + props.customClass
+                  : "features-item--panel-img-video"
+              }
+            >
+              <img
+                src={props.overlayImgSrc}
+                alt="img"
+                className="features-item--panel-img"
+              />
+
+              {props.downloadLoader ? (
+                <div className="stranger-animation">
+                  <div className="stranger-animation-image">
+                    <img
+                      alt=""
+                      src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/boxshot.png"
+                    />
+                  </div>
+                  <div className="stranger-animation-text">
+                    <div>Stranger Things</div>
+                    <div>Downloading...</div>
+                  </div>
+                </div>
+              ) : null}
+            </div>
+          )}
         </div>
       </div>
     </div>
