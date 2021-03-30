0b4ec56420c79a053dd024d97aa892ef5720349a
koexygen <rexarkoexygen@gmail.com>
2021-03-30 12:41:05 +0400

    landing page done mockup

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,12 +2,14 @@ import "./App.scss";
 import "./Browse/Browse";
 import Header from "./Header/Header";
 import LandingBody from "./LandingBody/LandingBody";
+import Footer from "./Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <LandingBody />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,223 @@
+import "./Footer.scss";
+import { FaGithub, FaLinkedin, FaFacebook, FaWhatsapp } from "react-icons/fa";
+import Gmail from "../../Assets/images/gmail.svg";
+import Instagram from "../../Assets/images/Instagram_logo_2016.svg";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer-title">
+        This netflix clone built by Gio Chomakhashvili for portfolio purpose.
+      </p>
+
+      <div className="footer--row">
+        <div className="footer--row-column">
+          <a
+            href="https://github.com/koexygen"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            Github <FaGithub style={{ fill: "#fff" }} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/gio-chomakhashvili-a739911b9/"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            LinkedIn <FaLinkedin style={{ fill: "#0e76a8" }} />
+          </a>
+          <a
+            href="https://www.facebook.com/thekoexygen/"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            Facebook <FaFacebook style={{ fill: "#4267B2" }} />
+          </a>
+          <a
+            href="https://www.instagram.com/koexygen/"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            Instagram
+            <img
+              src={Instagram}
+              alt=""
+              style={{ width: "1em", height: "1em" }}
+            />
+          </a>
+          <a
+            href="https://api.whatsapp.com/send?phone=995558888884&text=Hey%20Gio%2C%20"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            WhatsApp <FaWhatsapp style={{ fill: "#25D366" }} />
+          </a>
+
+          <a
+            href="mailto: ggiosweb@gmail.com"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            Gmail
+            <img src={Gmail} alt="" style={{ width: "1em", height: "1em" }} />
+          </a>
+        </div>
+        <div className="footer--row-column">
+          <a
+            href="https://github.com/koexygen"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            Github <FaGithub style={{ fill: "#fff" }} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/gio-chomakhashvili-a739911b9/"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            LinkedIn <FaLinkedin style={{ fill: "#0e76a8" }} />
+          </a>
+          <a
+            href="https://www.facebook.com/thekoexygen/"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            Facebook <FaFacebook style={{ fill: "#4267B2" }} />
+          </a>
+          <a
+            href="https://www.instagram.com/koexygen/"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            Instagram
+            <img
+              src={Instagram}
+              alt=""
+              style={{ width: "1em", height: "1em" }}
+            />
+          </a>
+          <a
+            href="https://api.whatsapp.com/send?phone=995558888884&text=Hey%20Gio%2C%20"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            WhatsApp <FaWhatsapp style={{ fill: "#25D366" }} />
+          </a>
+
+          <a
+            href="mailto: ggiosweb@gmail.com"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            Gmail
+            <img src={Gmail} alt="" style={{ width: "1em", height: "1em" }} />
+          </a>
+        </div>
+        <div className="footer--row-column">
+          <a
+            href="https://github.com/koexygen"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            Github <FaGithub style={{ fill: "#fff" }} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/gio-chomakhashvili-a739911b9/"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            LinkedIn <FaLinkedin style={{ fill: "#0e76a8" }} />
+          </a>
+          <a
+            href="https://www.facebook.com/thekoexygen/"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            Facebook <FaFacebook style={{ fill: "#4267B2" }} />
+          </a>
+          <a
+            href="https://www.instagram.com/koexygen/"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            Instagram
+            <img
+              src={Instagram}
+              alt=""
+              style={{ width: "1em", height: "1em" }}
+            />
+          </a>
+          <a
+            href="https://api.whatsapp.com/send?phone=995558888884&text=Hey%20Gio%2C%20"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            WhatsApp <FaWhatsapp style={{ fill: "#25D366" }} />
+          </a>
+
+          <a
+            href="mailto: ggiosweb@gmail.com"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            Gmail
+            <img src={Gmail} alt="" style={{ width: "1em", height: "1em" }} />
+          </a>
+        </div>
+        <div className="footer--row-column">
+          <a
+            href="https://github.com/koexygen"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            Github <FaGithub style={{ fill: "#fff" }} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/gio-chomakhashvili-a739911b9/"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            LinkedIn <FaLinkedin style={{ fill: "#0e76a8" }} />
+          </a>
+          <a
+            href="https://www.facebook.com/thekoexygen/"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            Facebook <FaFacebook style={{ fill: "#4267B2" }} />
+          </a>
+          <a
+            href="https://www.instagram.com/koexygen/"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            Instagram
+            <img
+              src={Instagram}
+              alt=""
+              style={{ width: "1em", height: "1em" }}
+            />
+          </a>
+          <a
+            href="https://api.whatsapp.com/send?phone=995558888884&text=Hey%20Gio%2C%20"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            WhatsApp <FaWhatsapp style={{ fill: "#25D366" }} />
+          </a>
+
+          <a
+            href="mailto: ggiosweb@gmail.com"
+            className="footer--row-column-link"
+            target="_blank"
+          >
+            Gmail
+            <img src={Gmail} alt="" style={{ width: "1em", height: "1em" }} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
