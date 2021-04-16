30e3ded9b01427c689eb60198c7edfebf79fcd55
koexygen <rexarkoexygen@gmail.com>
2021-04-16 16:41:45 +0400

    backup for keep working

--- src/Components/ProfilePick/ProfilePick.js
+++ src/Components/ProfilePick/ProfilePick.js
@@ -3,14 +3,18 @@ import Boy from "../../Assets/images/avatars/netflix-avatar.png";
 
 const ProfilePick = (props) => {
   return (
-    <div className="profile-pick">
-      <h1 className="profile-pick-title">Who's watching?</h1>
-      <ul className="profile-pick-list">
-        <li className="profile-pick-list-item">
-          <img src={Boy} alt="" className="profile-pick-list-item-avatar" />
-          <h3 className="profile-pick-list-item-name">Gio</h3>
-        </li>
-      </ul>
+    <div className="profile">
+      <div className="profile-pick">
+        <div className="profile-pick-container">
+          <div className="profile-pick-title">Who's watching?</div>
+          <ul className="profile-pick-list">
+            <li className="profile-pick-list-item">
+              <img src={Boy} alt="" className="profile-pick-list-item-avatar" />
+              <h3 className="profile-pick-list-item-name">Gio</h3>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
