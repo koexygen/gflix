7cd6a00b6492cf00d3c525b40d09072838f9ad12
koexygen <rexarkoexygen@gmail.com>
2021-04-16 16:19:20 +0400

    backup for keep working

--- src/Components/ProfilePick/ProfilePick.js
+++ src/Components/ProfilePick/ProfilePick.js
@@ -1,7 +1,18 @@
 import "./ProfilePick.scss";
+import Boy from "../../Assets/images/avatars/netflix-avatar.png";
 
 const ProfilePick = (props) => {
-  return <div>hi from profile</div>;
+  return (
+    <div className="profile-pick">
+      <h1 className="profile-pick-title">Who's watching?</h1>
+      <ul className="profile-pick-list">
+        <li className="profile-pick-list-item">
+          <img src={Boy} alt="" className="profile-pick-list-item-avatar" />
+          <h3 className="profile-pick-list-item-name">Gio</h3>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default ProfilePick;
