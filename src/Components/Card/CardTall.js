199b81789b5c9dc3f6a0f4c352a3e38017de11f7
koexygen <rexarkoexygen@gmail.com>
2021-04-29 15:37:23 +0400

    backup for move on

--- src/Components/Card/CardTall.js
+++ src/Components/Card/CardTall.js
@@ -1,15 +1,26 @@
 import React, { useState, useEffect } from "react";
 import "./CardTall.scss";
 
-const CardTall = (props) => {
+const CardTall = ({ card }) => {
   const [hover, setHover] = useState(false);
 
   const handleEnter = (e) => {
     setHover(true);
+    let event = e;
+
+    setTimeout(() => {
+      // debugger;
+      // event.target.play();
+    }, 500);
   };
 
   const handleLeave = (e) => {
     setHover(false);
+    let event = e;
+
+    setTimeout(() => {
+      // event.target.pause();
+    }, 500);
   };
 
   useEffect(() => {
@@ -21,14 +32,35 @@ const CardTall = (props) => {
       className="card-tall"
       onMouseEnter={handleEnter}
       onMouseLeave={handleLeave}
-      style={{ background: "red" }}
-    ></div>
+      style={{ transform: "scale(1.2)" }}
+    >
+      <div className="card-tall-content">
+        {/*<div className="card-tall-content-img">*/}
+        {/*  <img src={card.poster} alt={card.title} />*/}
+        {/*</div>*/}
+        <div className="card-tall-content-video">
+          <video
+            autoPlay
+            poster={card.poster}
+            src={card.trailerUrl}
+            onMouseOver={(event) => event.target.play()}
+            onMouseOut={(event) => event.target.pause()}
+          />
+        </div>
+      </div>
+    </div>
   ) : (
     <div
       className="card-tall"
       onMouseEnter={handleEnter}
       onMouseLeave={handleLeave}
-    ></div>
+    >
+      <div className="card-tall-content">
+        <div className="card-tall-content-img">
+          <img src={card.poster} alt={card.title} />
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/Components/Section/Section.js
+++ src/Components/Section/Section.js
@@ -1,14 +1,7 @@
 import "./Section.scss";
 import CardTall from "../Card/CardTall";
-import { getContent } from "../../Api";
 
 const Section = ({ allContent, tall }) => {
-  if (allContent) {
-    getContent(allContent[2].id).then((resp) => {
-      console.log(resp);
-    });
-  }
-
   const renderCards = (cards) => {
     return cards.map((card, i) => {
       return <CardTall card={card} key={i} />;
@@ -17,7 +10,7 @@ const Section = ({ allContent, tall }) => {
   return (
     <div
       className="section"
-      style={{ transform: `translateY(${tall ? "-45%" : "0"})` }}
+      style={{ transform: `translateY(${tall ? "-25%" : "0"})` }}
     >
       <ul className="section--inner">
         {allContent ? renderCards(allContent) : null}
