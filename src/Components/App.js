f089d2f6770e5b3fd109d2de58acaa22f418dc34
rexar <rexarkoexygen@gmail.com>
2021-05-02 03:42:06 +0400

    test

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,7 +10,7 @@ import SignUp from "./SignUp/SignUp";
 
 function App() {
   return (
-    <Router basename="/gflix">
+    <Router>
       <div className="App">
         <Route component={Navbar} />
 
